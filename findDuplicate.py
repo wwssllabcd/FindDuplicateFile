ab1626efbad8b1b4cef9d97d4039cc74ccd4b542
EricWang <wwssllabcd@gmail.com>
2020-01-05 01:31:11 +0800

    update code

--- findDuplicate.py
+++ findDuplicate.py
@@ -1,17 +1,21 @@
 
 import sys
 sys.path.append(r'D:\GitHub\Python')
-
 from EricCorePy.Utility.EricUtility import EricUtility
+
 u = EricUtility()
+if len(sys.argv) != 2:
+    exit
 
-path = r'E:\\Download\\tmp\\'
-fileDsc = path + r'\\dup\\'
+path = sys.argv[1]
+print('Start get list, path = ')
+print(path)
 
-u.make_folder(fileDsc)
+resultFolder = path + r'\\dup\\'
+u.make_folder(resultFolder)
 
 fileColls = u.get_duplicate_file_list(path)
 for file in fileColls:
-    u.move_file(path + file, fileDsc + file )
+    u.move_file(path + file, resultFolder + file )
 
 print('Finish')
