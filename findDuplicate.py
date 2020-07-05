95402d0f5dd1a2df5312ae842abb3c658a25c476
EricWang <a@a.a.a>
2020-07-05 14:51:29 +0800

    add code

--- findDuplicate.py
+++ findDuplicate.py
@@ -3,18 +3,20 @@ import sys
 sys.path.append(r'D:\GitHub\Python')
 from EricCorePy.Utility.EricUtility import EricUtility
 
-u = EricUtility()
 if len(sys.argv) != 2:
     exit
 
 path = sys.argv[1]
+path = path + r'\\'
 print('Start get list, path = ')
 print(path)
 
-resultFolder = path + r'\\dup\\'
+resultFolder = path + r"dup\\"
+
+u = EricUtility()
 u.make_folder(resultFolder)
 
-fileColls = u.get_duplicate_file_list(path)
+fileColls = u.get_duplicate_file_list_by_compare_file_data(path)
 for file in fileColls:
     u.move_file(path + file, resultFolder + file )
 
