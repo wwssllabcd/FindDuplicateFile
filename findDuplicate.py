c3e7eb89c582c9d947301f78329610f82cb8718b
EricWang <wwssllabcd@gmail.com>
2022-02-24 00:50:23 +0800

    miner change

--- findDuplicate.py
+++ findDuplicate.py
@@ -32,21 +32,27 @@ def get_duplicate_file_by_compare_file_data(fileDirGroupDirBySize):
     dupFileList = []
     compareSize = 512*1024
     for fileSize in fileDirGroupDirBySize:
-        sameSizefileList = fileDirGroupDirBySize[fileSize]
-        if len(sameSizefileList) < 2:
+        sameFileSizeGroup = fileDirGroupDirBySize[fileSize]
+        # 只處理兩個以上，且相同檔案大小的group
+        if len(sameFileSizeGroup) < 2:
             continue
 
-        naneAndDatas = []
-        for fileName in sameSizefileList:
-            naneAndDatas.append([fileName, u.get_file_data_binary(fileName, compareSize)])
+        #把相同檔案大小的檔案 group 中的每個檔案的資料讀出
+        fileNameAndDatas = []
+        for fileName in sameFileSizeGroup:
+            fileNameAndDatas.append([fileName, u.get_file_data_binary(fileName, compareSize)])
         
-        for compare in naneAndDatas:
-            for eachitem in naneAndDatas:
-                if compare[0] == eachitem[0]:
+        # 取出要 compare 的對象，使用兩個迴圈對每個檔案做比較
+        # 注意! 使用窮舉的方式，所有的檔案都會被標成記號，需要自己比較要留哪一個
+        for compare in fileNameAndDatas: # compare 的格式為 [fileName, data]
+            for file in fileNameAndDatas:
+                # 如果檔名相同，就跳過
+                if compare[0] == file[0]:
                     continue
 
                 print(compare[0])
-                if compare[1] == eachitem[1]:
+                # 檔名不同，且資料相同，則判定為 duplicate，最後所有重複的檔案都會被標成重複
+                if compare[1] == file[1]:
                     dupFileList.append(compare[0])
                     break
     return dupFileList
@@ -59,10 +65,13 @@ def get_file_name_by_full_path(fullPath):
     return res
 
 def rename_file(fileColls):
-    res = ""
+    res = "chcp 65001" + u.crlf()
     for file in fileColls:
         fileName = get_file_name_by_full_path(file)
-        res += 'ren "' + file + '" "_dup_' + fileName + '"' + u.crlf()
+        #加上 zzz 可保持排序在最後
+        res += 'ren "' + file + '" "ZZZZ_dup_' + fileName + '"' + u.crlf()
+        #res += 'move "' + file + '" E:\\Download\\tmp\\dup' + u.crlf()
+
     return res
 
 #------------------------
@@ -80,6 +89,8 @@ if __name__ == '__main__':
         print("No any path")
         exit()
 
+    #for debug
+    #paths.append("G:\CuteDL\mask_reserve")
     print(paths)
 
     fileColls = []
@@ -90,9 +101,12 @@ if __name__ == '__main__':
     fileDirGroupDirBySize = u.group_file_by_size(fileColls)
 
     dupFileList = get_duplicate_file_by_compare_file_data(fileDirGroupDirBySize)
+    print("make bat")
     dupFileList.sort()
     res = rename_file(dupFileList)
     u.to_file("renameFiles.bat", res)
 
     print(res)
     print('Finish')
+
+# 有重複的會被抓出來(連同本身)
